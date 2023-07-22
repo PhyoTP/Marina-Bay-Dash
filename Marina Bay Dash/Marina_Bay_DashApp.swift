ecc8fe9d154580859d589aa073f3a8d102fd5907
PhyoTP <phyo.thet.pai@sji.edu.sg>
2023-07-22 10:19:13 +0800

    Initial Commit

---
+++ Marina Bay Dash/Marina_Bay_DashApp.swift
@@ -0,0 +1,17 @@
+//
+//  Marina_Bay_DashApp.swift
+//  Marina Bay Dash
+//
+//  Created by T Krobot on 22/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Marina_Bay_DashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
